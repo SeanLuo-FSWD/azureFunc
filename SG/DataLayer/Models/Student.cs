39c39aef05e2f8d335ee69a04149cbbb9701b8a3
Sean Luo <shawnluo415@gmail.com>
2022-03-09 16:56:03 -0800

    ini

---
+++ SG/DataLayer/Models/Student.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Student {
+    public string StudentId { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string School { get; set; }
+    }
+
+}
